8ddc4cc1e2c3eacb38d5284ec51111cdd17f491d
Valeriy <valloskut@gmail.com>
2013-08-01 20:21:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Familyblogs::Application.config.secret_key_base = 'f6564b491615b7e6b6cb0a28ba8a7962427c6d7a1a7dd5eb46848e3a1ec495225f540d93052d42cefa57f3a93153495de7eb1623ad473b8119da9dac4df1c3fc'
